1b6d467fa78ecc4bb186da056e6321dc958933ad
jennyferbm13 <51867672+jennyferbm13@users.noreply.github.com>
2020-09-12 09:30:09 -0500

    Documentacion Control

--- src/juegoMemoria/ControlJuegoMemoria.java
+++ src/juegoMemoria/ControlJuegoMemoria.java
@@ -1,24 +1,53 @@
+/*
+ * Samuel Riascos 1922540
+ * Juan Camilo Randazzo 1923948
+ * Jennyfer Belalcazar 1925639
+ */
 package juegoMemoria;
 import java.util.ArrayList;
 import java.util.Random;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ControlJuegoMemoria. La clase controla todo el juego asigna un numero a cada cara, se determina si gano o perdio,
+ * me indica que caras se le estan visualizando al usuario y determina que caras se deben escoger dependiendo de la ronda
+ */
 public class ControlJuegoMemoria {
+	
+	/** The ronda. Indica en que ronda dl juego estamos*/
 	//Atributos
 	private int ronda;
+	
+	/** The numeros. En cada indice del arreglo se guarda un numero unico que se le asignara a cada cara del juego */
 	private ArrayList<Integer> numeros = new ArrayList<>();
+	
+	/** The caras. En cada indice se guarda un numero no repetido*/
 	private Cara[] caras;
+	
+	/** The aleatorio. Crear los numeros aleatoriamente */
 	private Random aleatorio;
+	
+	/** The indice escogido. Indice de la cara que se debe memorizar*/
 	private int indiceEscogido;
+	
+	/** The caras disponibles. Array de Caras que se le visualizando al usuario*/
 	private ArrayList<Cara> carasDisponibles = new ArrayList<Cara>();
 	
 	//Methods
 	
+	/**
+	 * Contructor, se inicia en ronda uno, se crean las doce caras del juego y objeto de tipo aleatorio
+	 * Instantiates a new control juego memoria.
+	 */
 	public ControlJuegoMemoria() {
 		ronda = 1;
 		caras = new Cara[12];
 		aleatorio = new Random();
 	}
-	//Genera números aleatorios únicos.
+	
+	/**
+	 * Generar numeros.  Genera nÃºmeros aleatorios Ãºnicos y se guarda n el array llamado numeros
+	 */
 	public void generarNumeros(){
 		boolean ocupado;
 		int candidato;
@@ -30,18 +59,21 @@ public class ControlJuegoMemoria {
 			numeros.add(candidato);
 		}
 	}
-	//Asigna un número único a cada cara.
+	
+	/**
+	 * Asignar un nÃºmero Ãºnico a cada cara que se guardo en el array numeros
+	 */
 	public void asignarNumeroACaras() {
 		for( int i = 0; i < 12; i++) {
 			caras[i] = new Cara(numeros.get(i));
 		}
 	}
-	public void mostrarCarasDisponibles() {
-		for(int i = 0; i < carasDisponibles.size(); i++) {
-			System.out.print(carasDisponibles.get(i).getNumero()+"\n");
-		}
-	}
-	//Define las caras disponibles en cada ronda.
+	
+	/**
+	 * Caras disponibles ronda. Define que caras se van visualizar en cada ronda
+	 *
+	 * @param cantidadCarasEscoger the cantidad caras escoger. Recibe como parametro cuaentas caras debe escoger dependiendo de la ronda
+	 */
 	public void carasDisponiblesRonda(int cantidadCarasEscoger) {
 		carasDisponibles.clear();
 		for (int i = 0; i < cantidadCarasEscoger; i++) {
@@ -49,19 +81,29 @@ public class ControlJuegoMemoria {
 		}
 	}
 	
-	//Escoge la cara que el jugador debe adivinar.
+	/**
+	 * Escoge la cara que el jugador debe adivinar.
+	 */
 	public void escogerCaraAJugar() {
 		 int size = carasDisponibles.size();
-		 System.out.print("Tamano caras disponibles "+size+"\n");
 		 indiceEscogido = aleatorio.nextInt(size);
 	}
+	
+	/**
+	 * Retornar el indice de la cara que se debe memorizar.
+	 *
+	 * @return the int
+	 */
 	public int retornarNumeroCaraJugar() {
 		return indiceEscogido;
 	}
-	public int retornarElemento() {
-		return carasDisponibles.get(indiceEscogido).getNumero();
-	}
 	
+	/**
+	 * Perdio O gano.
+	 *
+	 * @param numeroEscogidoUsuario, es el indice de la imagen que escogio el usuario 
+	 * @return true o falso, si es true se pasa de ronda y se vuelven a esocoger las cara aleatoriamente a jugar
+	 */
 	public boolean perdioOGano(int numeroEscogidoUsuario) {
 		int carasAEscoger = determinarCarasEscoger();
 		carasDisponiblesRonda(carasAEscoger);
@@ -74,7 +116,13 @@ public class ControlJuegoMemoria {
 		}
 		return false;
 	}
-	//Cuántas caras podemos escoger.
+	
+	/**
+	 * Determinar caras escoger dependiendo de que ronda se esta jugando
+	 *
+	 * @return un tipo de dato int(cantidad caras que se van a visualizar por ronda)
+	 */
+	//CuÃ¡ntas caras podemos escoger.
 	public int determinarCarasEscoger() {
 		int cantidadCarasEscoger = 0;
 			switch(ronda) {
@@ -98,6 +146,13 @@ public class ControlJuegoMemoria {
 					return cantidadCarasEscoger;
 			}	
 	}
+	
+	/**
+	 * Gets the cara.
+	 *
+	 * @param recibe un indice
+	 * @return retorna el numero que se guardo en esa cara
+	 */
 	public int getCara(int indice) {
 		return caras[indice].getNumero();
 	}
